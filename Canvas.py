9bb02c4dd0915a8b7d1c66d250b6a58ed648b9be
Michelle Cheung <micheung@berkeley.edu>
2017-10-07 13:40:38 -0700

    Tile and Block classes, display images with Canvas

--- Canvas.py
+++ Canvas.py
@@ -10,3 +10,16 @@ class Canvas(tk.Canvas):
     def clear(self):
         self.delete("all")
         self.pack()
+
+top = tk.Tk()
+C = tk.Canvas(top, bg="blue", height=340, width=360)
+C2 = tk.Canvas(top, bg="blue", height=578, width=883)
+filename = "Sprites/SNES - Super Mario World - Objects & Blocks.gif"
+filename2 = "Sprites/SNES - Super Mario World - Ground Tiles.gif"
+img_file = tk.PhotoImage(file=filename)
+img_file2 = tk.PhotoImage(file=filename2)
+image = C.create_image(185, 175, image=img_file)
+image2 = C2.create_image(445, 180, image=img_file2)
+C.pack()
+C2.pack()
+top.mainloop()
--- PPU.py
+++ PPU.py
@@ -1,21 +1,49 @@
 from RAM import RAM
 
-
 class PPU:
     cpu_ram = None
     ram = None
+    xpixels, ypixels = 256, 240
     def __init__(self, cpu_ram: RAM, ppu_ram: RAM):
         self.cpu_ram = cpu_ram
         self.ram = ppu_ram
 
-    
 
-        
+"""A tile is an 8x8 region."""
+class Tile:
+    xpixels, ypixels = 8, 8
+    def __init__(self, x, y, filename):
+        self.x, self.y = x, y
+        self.filename = filename
+
+
+"""A block is a 16x16 region containing four tiles."""
+class Block:
+    width, height = 2, 2
+    def __init__(self, tiles):
+        self.tiles = tiles
+
+    def get_tiles(self):
+        return self.tiles
+
+    def ULtile(self):
+        return self.tiles[0]
+
+    def URtile(self):
+        return self.tiles[1]
+
+    def LLtile(self):
+        return self.tiles[2]
+
+    def LRtile(self):
+        return self.tiles[3]
+
+
 """A nametable is the data structure for a background."""
 class Nametable:
+    width, height = 32, 30
     def __init__(self, grid):
         self.grid = grid
-        self.width, self.height = len(grid[0]), len(grid)
         self.tiles = self.all_tiles()
 
     """Returns a list of the tile numbers in the nametable."""
